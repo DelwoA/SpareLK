c14cdd28a7ed4a725821c1bd05d692c4afe6a406
Delwo Athauda <delwo2003@gmail.com>
2025-05-13 03:16:28 +0530

    Refactor Li, Slide, Slider, and Button components for improved styling and functionality. Updated Li component with enhanced class names and structure. Redesigned Slide component with better text readability and added button component for actions. Enhanced Slider component layout and responsiveness. Standardized imports and formatting across components.

--- src/components/Li.tsx
+++ src/components/Li.tsx
@@ -1,28 +1,31 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { Link } from "react-router-dom"
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 
 type LiProps = {
-    to: string,
-    children: React.ReactNode,
-    onClick?: () => void,
-    icon?: any,
-    currentPath?: string
-}
+  to: string;
+  children: React.ReactNode;
+  onClick?: () => void;
+  icon?: any;
+  currentPath?: string;
+};
 
-export default function Li({ children, to, icon, onClick, currentPath }: LiProps) {
-    return (
-        <Link
-            id='nav-link'
-            to={to}
-            onClick={onClick}
-            className={`block px-4 py-2 hover:bg-main/50 hover:text-black hover:brightness-125 rounded hover:translate-x-3 ease-in duration-75 
-                ${currentPath === to && 'bg-main text-light hover:bg-main'}`}
-        >
-            {
-                icon && <FontAwesomeIcon className='mr-2' icon={icon} />
-            }
-            {children}
-        </Link>
-    )
+export default function Li({
+  children,
+  to,
+  icon,
+  onClick,
+  currentPath,
+}: LiProps) {
+  return (
+    <Link
+      id="nav-link"
+      to={to}
+      onClick={onClick}
+      className={`block px-4 py-2 text-slate-700 text-sm hover:bg-accent hover:text-accent-foreground rounded ease-in duration-75 
+                ${currentPath === to && "bg-muted text-muted-foreground"}`}
+    >
+      {icon && <FontAwesomeIcon className="mr-2 text-slate-700" icon={icon} />}
+      {children}
+    </Link>
+  );
 }
